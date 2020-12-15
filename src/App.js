bbb99e9d142012add4968bd6bc24187ffc968089
Keshav <keshavnaganathan@gmai.com>
2020-12-15 20:01:33 +0530

    Added all changes to files and removed files which are not to be used

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
+import { data } from "./data";
+var songList = Object.keys(data);
+
+export default function App() {
+  var [genre, setGenre] = useState("Slow_Melodies");
+  function genreHandler(genre) {
+    setGenre(genre);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>What is your mood?</h1>
+        <h2>Choose from the below categories of music depending on your mood</h2>
+        <div className="buttons">
+          {songList.map((genre) => {
+            return <button onClick={() => genreHandler(genre)}>{genre}</button>;
+          })}
+        </div>
       </header>
+      <div className="songs">
+        {data[genre].map((song) => {
+          return (
+            <div className="song" key={song.name}>
+              <script
+                defer
+                src="https://use.fontawesome.com/releases/v5.15.1/js/all.js"
+                integrity="sha384-9/D4ECZvKMVEJ9Bhr3ZnUAF+Ahlagp1cyPC7h5yDlZdXs4DQ/vRftzfd+2uFUuqS"
+                crossorigin="anonymous"
+              ></script>
+              <img className="songImg" alt="Song Name" src={song.imgURL}></img>
+              <div className="songContent">
+                <h4> {song.name}</h4>
+                <p> {song.singer}</p>
+                <a href={song.songURL} target="">
+                  Listen on Gaana 🎵 
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,101 @@
+export const data = {
+    Slow_Melodies: [
+      {
+        name: "Lovely",
+        singer: "Bille Eillish",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/28/2121928/crop_175x175_2121928.jpg",
+        songURL: "https://gaana.com/song/lovely-128"
+      },
+      {
+        name: "So Far Away",
+        singer: "Martin Garrix",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/55/2031655/crop_175x175_1512364614_2031655.jpg",
+        songURL: "https://gaana.com/album/so-far-away-english-2017"
+      },
+      {
+        name: "Paris",
+        singer: "The Chainsmokers",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/12/1843312/crop_175x175_1843312.jpg?version=1",
+        songURL:
+          "https://gaana.com/album/paris-english-2017"
+      },
+      
+    ],
+    Motivational: [
+      {
+        name: "Lose Yourself",
+        singer: "Eminem",
+        imgURL:
+          "https://a10.gaanacdn.com/images/playlists/7/439307/crop_175x175_1587110833_439307.jpg",
+        songURL: "https://gaana.com/playlist/gaana-dj-best-of-eminem"
+      },
+      {
+        name: "Beleiver",
+        singer: "Imagine Dragons",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/80/1925480/crop_175x175_1925480.jpg",
+        songURL: "https://gaana.com/song/believer-173"
+      },
+      {
+        name: "In the End",
+        singer: "Linkin Park",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/14/186814/crop_175x175_186814.jpg",
+        songURL: "https://gaana.com/album/hybrid-theory"
+      },
+      
+    ],
+    Party: [
+      {
+        name: "Magenta Riddim",
+        singer: "Dj Snake",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/45/2076445/crop_175x175_2076445.jpg",
+        songURL: "https://gaana.com/album/magenta-riddim"
+      },
+      {
+        name: "Cheap Thrills",
+        singer: "Sia",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/18/1682218/crop_175x175_1682218.jpg",
+        songURL: "https://gaana.com/album/cheap-thrills-english-2016"
+      },
+      {
+        name: "Despacito",
+        singer: "Luis Fonsi",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/73/1842473/crop_175x175_1842473.jpg",
+        songURL: "https://gaana.com/album/cheap-thrills-english-2016"
+      },
+      
+    ],
+    EDM: [
+      {
+        name: "Spectre",
+        singer: "Alan Walker",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/13/1988313/crop_175x175_1988313.jpg",
+        songURL: "https://gaana.com/song/the-spectre-9"
+      },
+      {
+        name: "Dangerous",
+        singer: "David Guetta",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/84/241984/crop_175x175_241984.jpg",
+        songURL: "https://gaana.com/album/dangerous-feat-sam-martin"
+      },
+      {
+        name: "Faded",
+        singer: "Alan Walker",
+        imgURL:
+          "https://a10.gaanacdn.com/images/albums/62/1846262/crop_175x175_1846262.jpg",
+        songURL: "https://gaana.com/song/faded-156"
+      },
+      
+    ],
+   
+  };
+  
